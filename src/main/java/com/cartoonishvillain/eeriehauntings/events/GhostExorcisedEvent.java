394156402c9261322df3fc639868d9221fa12302
Cartoonish Villain <jedijoe93@gmail.com>
2021-10-21 15:20:00 -0400

    Documents the event added in PR #1

--- src/main/java/com/cartoonishvillain/eeriehauntings/events/GhostExorcisedEvent.java
+++ src/main/java/com/cartoonishvillain/eeriehauntings/events/GhostExorcisedEvent.java
@@ -11,6 +11,15 @@ public class GhostExorcisedEvent extends Event {
     private final int ghostType;
     private final boolean isExpell;
 
+    /**
+     * This event is fired when a player actively exorcises a ghost themselves.
+     * Called in {@link ForgeBusEvents#exorciseGhost(ServerPlayer)} and {@link ForgeBusEvents#expellGhost(ServerPlayer)}.<br>
+     * <br>
+     * {@link #ghostType} refers to the type of ghost (and therefore the method needed) to exorcise the spirit.<br>
+     * <br>
+     * {@link #isExpell} refers to if the player exorcised the ghost while it was angered with another method. This isn't rewarded within the mod when true.
+     **/
+
     public GhostExorcisedEvent(ServerPlayer player, int ghostType, boolean isExpell) {
         this.player = player;
         this.ghostType = ghostType;
